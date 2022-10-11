1904e9de049944aec4231a4be98fb7b5ac56ecb7
Sam Avery <sam.avery1@gmail.com>
2022-10-11 17:50:03 -0500

    commit tree a50618ca7bbc8d704b68927621f83797a0897d83

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        # [array[0..-1].join(", "), array.last].join(" and ")
+        # array.insert(-2, "and")
+        # array[0..-1].join(", ")
 
+        new_array = array.slice(0..-2).join(", ")
+        # last_element = array.last
+        new_array << ", and " << array.last
+    else 
+        array.join
+    end
 end
